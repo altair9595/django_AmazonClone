a5004c32b3d4efe89f04ee24153e044b37a16eed
altair9595 <mhdfadialtair@gmail.com>
2023-07-03 16:39:04 +0200

    desung products moels

--- products/models.py
+++ products/models.py
@@ -6,24 +6,23 @@ from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class Product(models.Model):
-    name =models.CharField(max_length=120)
-    price = models.FloatField()
-    quantity = models.IntegerField()
-    description = models.TextField(max_length=30000)
-    subtitle = models.TextField(max_length=500)
-    sku = models.IntegerField()
-    brand =models.ForeignKey('Brand',related_name='product_brand', on_delete=models.SET_NULL,null=try,blank=True)
+    name =models.CharField(_('name'),max_length=120)
+    price = models.FloatField(_('price'))
+    quantity = models.IntegerField(_('quantity'))
+    description = models.TextField(_('description'),max_length=30000)
+    subtitle = models.TextField(_('subtitle'), max_length=500)
+    sku = models.IntegerField(_('sky'))
+    brand =models.ForeignKey('Brand',verbose_name=_('brand'),related_name='product_brand', on_delete=models.SET_NULL,null=True,blank=True)
     tags = TaggableManager()
-    image = models.ImageField(upload_to='products')
-    flag = models.CharField(max_length=10 , choices=FLAG_TYPES , default='New')
-    
+    image = models.ImageField(_('image'),upload_to='products')
+  
     def __str__(self):
         return self.name
 
 
 class ProductImages(models.Model):
      product = models.ForeignKey(Product,verbose_name=('product'),related_name='product_image',on_delete=models.CASCADE)
-     image = models.ImageField(upload_to='productimages')
+     image = models.ImageField(_('image'),upload_to='productimages')
     
      def __str__(self):
         return str(self.product)
@@ -31,8 +30,8 @@ class ProductImages(models.Model):
 
 
 class Brand(models.Model):
-    name =models.CharField(max_length=120)
-    imag =models.ImageField(upload_to='brands')
+    name =models.CharField(_('name'),max_length=120)
+    image =models.ImageField(_('image'),upload_to='brands')
 
 
     def __str__(self):
@@ -40,11 +39,11 @@ class Brand(models.Model):
     
 
 class Review(models.Model):
-    user = models.ForeignKey(User,related_name='review_author',on_delete=models.CASCADE)
-    Product = models.ForeignKey(Product,related_name='product_review',on_delete=models.CASCADE)
-    review =models.TextField(max_length=500)
-    rate =models.BigIntegerField
-    create_date =models.DateTimeField(default=timezone.now)
+    user = models.ForeignKey(User,verbose_name=('user'),related_name='review_author',on_delete=models.CASCADE)
+    Product = models.ForeignKey(Product,verbose_name=('product'),related_name='product_review',on_delete=models.CASCADE)
+    review =models.TextField(_('review'),max_length=500)
+    rate =models.BigIntegerField(_('rate'))
+    create_date =models.DateTimeField(_('creat_date'),default=timezone.now)
 
     def __str__(self):
         return str(self.Product)
